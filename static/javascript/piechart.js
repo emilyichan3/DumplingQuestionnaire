70d04d90c0200de4bd16bdea947c975ff49577e7
Yi-Jung Chen <emilyichan3@gmail.com>
2024-09-12 21:27:54 +0100

    Created dynamic pieChart by each id

--- static/javascript/piechart.js
+++ static/javascript/piechart.js
@@ -1,40 +1,45 @@
-// var abc = document.getElementById("id-1").innerText;
+
 function toTrimString(s) {
     return s.trim();
 }
 function toInteger(s) {
     return parseInt(s.trim())
 }
+var allIds = document.getElementsByClassName('hide-id');
 
-var target = 'pie-1';
+//using allIds.length to know how many question been shown
+var targetId = allIds.length;
+var target = 'pie-' + targetId;
 
-var test = document.getElementById(target);
-var ctx = document.getElementById(target).getContext("2d");
-var question = document.getElementById("question-1").innerText;
-var keys = document.getElementById("keys-1").innerText;
-var values = document.getElementById("values-1").innerText;
-
-var pieChart = new Chart(ctx, {
-    type: "pie",
-    data: {
-        // labels: ['never', 'sometimes', 'often', 'very', 'almost'],
-        labels: keys.slice(1,-1).replaceAll("'","").split(",").map(toTrimString),
-        datasets: [{
-            textinfo: "label+percent",
-            backgroundColor: barColors,
-            data: values.slice(1,-1).replaceAll("'","").split(",").map(toInteger),
-        }]
-    },
-    options: {
-        responsive: false,
-        plugins: {
-            title: {
-            display: true,
-            text: question
+if (targetId) {
+    var ctx = document.getElementById(target).getContext("2d");
+    var question = document.getElementById("question-"+targetId).innerText;
+    var keys = document.getElementById("keys-"+targetId).innerText;
+    var values = document.getElementById("values-"+targetId).innerText;
+    
+    var pieChart = new Chart(ctx, {
+        type: "pie",
+        data: {
+            // labels: ['never', 'sometimes', 'often', 'very', 'almost'],
+            labels: keys.slice(1,-1).replaceAll("'","").split(",").map(toTrimString),
+            datasets: [{
+                textinfo: "label+percent",
+                backgroundColor: barColors,
+                data: values.slice(1,-1).replaceAll("'","").split(",").map(toInteger),
+            }]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                title: {
+                display: true,
+                text: question
+                }
             }
         }
-    }
-});      
+    });    
+}
+  
 
 // jQuery
 // $(pieChart).appendTo("#pie-1");
